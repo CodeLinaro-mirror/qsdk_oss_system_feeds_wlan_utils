96113e66e5bdababa81be5927f4842f28a06294d
Kannan Saravanan <quic_kannsara@quicinc.com>
2023-09-08 15:53:42 +0530

    wifi-scripts: Create new read_caldata_to_fs.sh for 16M Profile
    Create new read_caldata_to_fs.sh script that supports only 16M Profile RDPs.
    
    Signed-off-by: Kannan Saravanan <quic_kannsara@quicinc.com>
    Change-Id: I0e55dc2139678909fefe67f2a207c86c041b3d35

---
+++ wifi-scripts/files/read_caldata_to_fs_16m.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# Copyright (c) 2015, 2020, The Linux Foundation. All rights reserved.
+# Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+. /lib/functions.sh
+. /lib/create_cfg_caldata.sh
+
+do_load_ipq4019_board_bin()
+{
+
+    local board=ap$(echo $(board_name) | awk -F 'ap' '{print$2}')
+    local mtdblock=$(find_mtd_part 0:ART)
+
+    local apdk="/tmp"
+
+    if [ -z "$mtdblock" ]; then
+        # read from mmc
+        mtdblock=$(find_mmc_part 0:ART)
+    fi
+
+    [ -n "$mtdblock" ] || return
+
+    # load board.bin
+    case "$board" in
+            ap-mi01.3*|ap-mi04.1*)
+                    [ -f /lib/firmware/IPQ5332/caldata.bin ] && return
+                    mkdir -p ${apdk}/IPQ5332
+                    mkdir -p ${apdk}/qcn6432
+                    do_ftm_conf_override
+
+                    create_cfg_caldata "${mtdblock}" "IPQ5332" "qcn6432"
+            ;;
+            ap-mi*)
+                    [ -f /lib/firmware/IPQ5332/caldata.bin ] && return
+                    mkdir -p ${apdk}/IPQ5332
+                    create_cfg_caldata "${mtdblock}" "IPQ5332"
+            ;;
+   esac
+}
+
