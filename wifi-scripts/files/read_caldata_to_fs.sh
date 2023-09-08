e5a43777f2de1b93f99c698963dd2f0bbc6e5e9d
Kannan Saravanan <quic_kannsara@quicinc.com>
2023-09-08 15:53:42 +0530

    wifi-scripts: Create new read_caldata_to_fs.sh for 16M Profile
    Create new read_caldata_to_fs.sh script that supports only 16M Profile RDPs.
    
    Signed-off-by: Kannan Saravanan <quic_kannsara@quicinc.com>
    Change-Id: I0e55dc2139678909fefe67f2a207c86c041b3d35
    (cherry picked from commit 96113e66e5bdababa81be5927f4842f28a06294d)

--- wifi-scripts/files/read_caldata_to_fs.sh
+++ wifi-scripts/files/read_caldata_to_fs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # Copyright (c) 2015, 2020, The Linux Foundation. All rights reserved.
+# Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
 
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -15,6 +16,7 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 . /lib/functions.sh
+. /lib/create_cfg_caldata.sh
 
 is_ftm_conf_supported() {
 	local board=ap$(echo $(board_name) | awk -F 'ap' '{print$2}')
@@ -30,84 +32,6 @@ is_ftm_conf_supported() {
 }
 
 is_ftm_conf_supported
-
-do_ftm_conf_override()
-{
-        #Necessary conditon check, This method will be invoked only for Miami+Pebble RDP's
-        #Inside this API, we will update the ftm.conf file with DTS board ID values maintained.
-        #This is applicable only for IPA RDP's, For other M+P RDP's return [Do nothing]
-        local board=ap$(echo $(board_name) | awk -F 'ap' '{print$2}')
-
-        case "$board" in
-                ap-mi04.1*)
-                        ;;
-                *)
-                        echo "Board name is $board -do_ftm_conf_override API not applicable" > /dev/console && return
-                ;;
-        esac
-
-        local board_id_5g=`hexdump -C /proc/device-tree/soc/wifi4@f00000/qcom,board_id | awk '{print $5}'`
-        local board_id_6g=`hexdump -C /proc/device-tree/soc/wifi5@f00000/qcom,board_id | awk '{print $5}'`
-
-        awk -F',' -v board=$board -v board_id_5g=$board_id_5g -v board_id_6g=$board_id_6g '{
-                if ($1 == board) {
-                        print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" NR
-                        lineNumber=NR
-                        if ($3 == 1){
-                                print "5G slot Instance -lineNumber" lineNumber "DTS board ID - "board_id_5g
-                                cmd = "sed -i " lineNumber"s" "\/" $2 "\/" "00" board_id_5g "\/" " /ini/ftm.conf"
-                        }
-                        else if($3 == 2)
-                        {
-                                print "6G slot Instance -lineNumber" lineNumber "DTS board ID - "board_id_6g
-                                cmd = "sed -i " lineNumber"s" "\/" $2 "\/" "00" board_id_6g "\/" " /ini/ftm.conf"
-                        }
-                        system(cmd)
-                }
-        }' /ini/ftm.conf
-}
-
-create_cfg_caldata() {
-	local brd=ap$(echo $(board_name) | awk -F 'ap' '{print$2}')
-
-	awk -F ',' -v apdk='/tmp/' -v mtdblock=$1 -v ahb_dir=$2 -v pci_dir=$3 -v is_wkk=$4 -v board=$brd '{
-		if ($1 == board) {
-			print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5
-			if ($3 == 0) {
-				print "Internal radio"
-				cmd ="stat -Lc%s /lib/firmware/" ahb_dir "/bdwlan.b" $2 " 2> /dev/null"
-				cmd | getline BDF_SIZE
-				close(cmd)
-				if(!BDF_SIZE) {
-					print "BDF file for Board id " $2 " not found. Using default value"
-					BDF_SIZE=131072
-				}
-				cmd = "dd if="mtdblock" of=" apdk ahb_dir "/caldata.bin bs=1 count=" BDF_SIZE " skip=" $4
-				system(cmd)
-				cmd = "cp " apdk ahb_dir "/caldata.bin /lib/firmware/" ahb_dir "/"
-				system(cmd)
-			} else {
-				print "PCI radio"
-				cmd ="stat -Lc%s /lib/firmware/" pci_dir "/bdwlan.b" $2 " 2> /dev/null"
-				cmd | getline BDF_SIZE
-				close(cmd)
-				if(!BDF_SIZE) {
-					print "BDF file for Board id " $2 " not found. Using default value"
-					if (is_wkk == 1)
-						BDF_SIZE=184320
-					else
-						BDF_SIZE=131072
-				}
-				cmd = "dd if="mtdblock" of=" apdk pci_dir "/caldata_" $3 ".b" $2 " bs=1 count=" BDF_SIZE " skip=" $4
-				system(cmd)
-				cmd = "cp " apdk pci_dir "/caldata_" $3 ".b" $2 " /lib/firmware/" pci_dir "/"
-				system(cmd)
-			}
-		}
-	}' /ini/ftm.conf
-
-	[ -f /lib/firmware/$2/caldata.bin ] || touch /lib/firmware/$2/caldata.bin
-}
 
 do_load_ipq4019_board_bin()
 {
