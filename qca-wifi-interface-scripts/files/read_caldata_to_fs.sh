aa274a61a921bf302b6039bb6cfac4333f397e18
Aarthy Mohanraj <quic_aartym@quicinc.com>
2025-06-16 23:28:59 +0530

    qca-wifi: Caldata retrieval changes for RDP461
    Change-Id: I9feef3eed9a18dad0d6dea9565b6bf71e8190bf3
    Signed-off-by: Aarthy Mohanraj <quic_aarthym@quicinc.com>

--- qca-wifi-interface-scripts/files/read_caldata_to_fs.sh
+++ qca-wifi-interface-scripts/files/read_caldata_to_fs.sh
@@ -24,7 +24,7 @@ is_ftm_conf_supported() {
         local ftm_conf_path=$(get_config_file_path "caldata")
 
 	case "$board" in
-	ap-mi*|ap-al02-c4*|ap-al02-c6*|ap-al06*|ap-al05*|ap-al02-c7*|ap-al02-c8*|ap-al02-c9*|ap-al02-c10*|ap-al02-c11*|ap-al02-c12*|ap-al02-c14*|ap-al02-c15*|ap-al02-c16*|ap-al02-c20*|ap-al03-c1*|ap-al03-c2*|db-mi02.1*|ap-sdxpinn-qcn9224*|rdp466*|rdp485*|rdp487*|tb-mi03.1*|tb-mi05.1*|rdp496*|ap-al02-c13*)
+	ap-mi*|ap-al02-c4*|ap-al02-c6*|ap-al06*|ap-al05*|ap-al02-c7*|ap-al02-c8*|ap-al02-c9*|ap-al02-c10*|ap-al02-c11*|ap-al02-c12*|ap-al02-c14*|ap-al02-c15*|ap-al02-c16*|ap-al02-c20*|ap-al03-c1*|ap-al03-c2*|db-mi02.1*|ap-sdxpinn-qcn9224*|rdp466*|rdp485*|rdp487*|tb-mi03.1*|tb-mi05.1*|rdp496*|ap-al02-c13*|ap-al02-c19*)
 		ln -s $ftm_conf_path/ftm.conf /tmp/ftm.conf
 		;;
 	*)
@@ -321,7 +321,7 @@ do_load_ipq4019_board_bin()
 
                     create_cfg_caldata_mr "${mtdblock}" "IPQ9574"
             ;;
-            ap-al02-c4*|ap-al02-c6*|ap-al06*|ap-al05*|ap-al02-c7*|ap-al02-c8*|ap-al02-c9*|ap-al02-c10*|ap-al02-c11*|ap-al02-c12*|ap-al02-c14*|ap-al02-c15*|ap-al02-c16*|ap-al02-c20*|ap-al03-c1*|ap-al03-c2*)
+            ap-al02-c4*|ap-al02-c6*|ap-al06*|ap-al05*|ap-al02-c7*|ap-al02-c8*|ap-al02-c9*|ap-al02-c10*|ap-al02-c11*|ap-al02-c12*|ap-al02-c14*|ap-al02-c15*|ap-al02-c16*|ap-al02-c20*|ap-al03-c1*|ap-al03-c2*|ap-al02-c19*)
                     [ -f /lib/firmware/IPQ9574/caldata.bin ] && return
                     mkdir -p ${apdk}/IPQ9574
                     mkdir -p ${apdk}/qcn9224
