156882a7a71d91c5467112a6dbedde388e115c5b
Aarthy Mohanraj <aarthym@qti.qualcomm.com>
2025-09-30 15:37:58 +0530

    wlan_utils: ftm.conf override script changes to generate caldata using fdtedit command[RDP433]
    Change-Id: Ic661cb4bb8e9b35122c55603058dfcdbe8779b98
    Signed-off-by: Aarthy Mohanraj <aarthym@qti.qualcomm.com>

--- qca-wifi-interface-scripts/files/read_caldata_to_fs.sh
+++ qca-wifi-interface-scripts/files/read_caldata_to_fs.sh
@@ -321,7 +321,15 @@ do_load_ipq4019_board_bin()
 
                     create_cfg_caldata_mr "${mtdblock}" "IPQ9574"
             ;;
-            ap-al02-c4*|ap-al02-c6*|ap-al06*|ap-al05*|ap-al02-c7*|ap-al02-c8*|ap-al02-c9*|ap-al02-c10*|ap-al02-c11*|ap-al02-c12*|ap-al02-c14*|ap-al02-c15*|ap-al02-c16*|ap-al02-c20*|ap-al03-c1*|ap-al03-c2*|ap-al02-c19*)
+            ap-al02-c4*)
+                [ -f /lib/firmware/IPQ9574/caldata.bin ] && return
+                    mkdir -p ${apdk}/IPQ9574
+                    mkdir -p ${apdk}/qcn9224
+                    do_ftm_conf_override
+
+                    create_cfg_caldata "${mtdblock}" "IPQ9574" "qcn9224" "0" 
+            ;;
+            ap-al02-c6*|ap-al06*|ap-al05*|ap-al02-c7*|ap-al02-c8*|ap-al02-c9*|ap-al02-c10*|ap-al02-c11*|ap-al02-c12*|ap-al02-c14*|ap-al02-c15*|ap-al02-c16*|ap-al02-c20*|ap-al03-c1*|ap-al03-c2*|ap-al02-c19*)
                     [ -f /lib/firmware/IPQ9574/caldata.bin ] && return
                     mkdir -p ${apdk}/IPQ9574
                     mkdir -p ${apdk}/qcn9224
