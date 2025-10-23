0f92c1f183ef7582fb522fb0deec1146e2f193e0
Aarthy Mohanraj <aarthym@qti.qualcomm.com>
2025-10-23 16:54:08 +0530

    [wlan_utils] Modified caldata download script to have proper naming convention for 2G caldata
    Change-Id: If5663b29ed8b4a152aa80830196ce3e3662febdb
    Signed-off-by: Aarthy Mohanraj <aarthym@qti.qualcomm.com>

--- qca-wifi-interface-scripts/files/create_cfg_caldata.sh
+++ qca-wifi-interface-scripts/files/create_cfg_caldata.sh
@@ -133,6 +133,7 @@ do_ftm_conf_override()
         local board_id_5g
         local board_id_6g
         local ker_ver=`uname -r |cut -d. -f1`
+        #Check for presence of ATH module to differentiate the folder read of dts file b/w PROP and ATH
         ath12k="/etc/modules.d/ath12k"
         if [ $ker_ver -ge 6 ]; then
             case "$board" in
@@ -146,15 +147,15 @@ do_ftm_conf_override()
                         ;;
                         esac
                     ;;
-                    ap-al02-c4)
-                    if [ -e "$ath12k" ]; then
-                        board_id_2g=`hexdump -C /proc/device-tree/soc@0/pci@10000000/pcie@0/wifi@0/qcom,board_id | awk '{print $5}'`
-                        board_id_5g=`hexdump -C /proc/device-tree/soc@0/pci@18000000/pcie@0/wifi@0/qcom,board_id | awk '{print $5}'`
-                        board_id_6g=`hexdump -C /proc/device-tree/soc@0/pci@20000000/pcie@0/wifi@0/qcom,board_id | awk '{print $5}'`
-                    else
-                        board_id_2g=`hexdump -C /proc/device-tree/soc@0/wifi5@f00000/board_id | awk '{print $5}'`
-                        board_id_5g=`hexdump -C /proc/device-tree/soc@0/wifi7@f00000/board_id | awk '{print $5}'`
-                        board_id_6g=`hexdump -C /proc/device-tree/soc@0/wifi6@f00000/board_id | awk '{print $5}'`
+                    ap-al02-c4) #check for RDP433
+                    if [ -e "$ath12k" ]; then   #override for ATH   
+                        board_id_2g=`hexdump -C /proc/device-tree/soc@0/pci@10000000/pcie@0/wifi@0/qcom,board_id | awk '{print $5}'`    #Read dts file for 2G board_id
+                        board_id_5g=`hexdump -C /proc/device-tree/soc@0/pci@18000000/pcie@0/wifi@0/qcom,board_id | awk '{print $5}'`    #Read dts file for 5G board_id
+                        board_id_6g=`hexdump -C /proc/device-tree/soc@0/pci@20000000/pcie@0/wifi@0/qcom,board_id | awk '{print $5}'`    #Read dts file for 6G board_id
+                    else    #override for PROP
+                        board_id_2g=`hexdump -C /proc/device-tree/soc@0/wifi5@f00000/board_id | awk '{print $5}'`   #Read dts file for 2G board_id
+                        board_id_5g=`hexdump -C /proc/device-tree/soc@0/wifi7@f00000/board_id | awk '{print $5}'`   #Read dts file for 5G board_id
+                        board_id_6g=`hexdump -C /proc/device-tree/soc@0/wifi6@f00000/board_id | awk '{print $5}'`   #Read dts file for 6G board_id
                     fi
                     ;;
                     *)
@@ -203,22 +204,31 @@ do_ftm_conf_override()
                     }
                 }' $ftm_conf_path/ftm.conf
             ;;
-            ap-al02-c4)
+            ap-al02-c4) #Update ftm.conf file with the overridden board_id for RDP433
                 awk -F',' -v board=$board -v board_id_2g=$board_id_2g -v board_id_5g=$board_id_5g -v board_id_6g=$board_id_6g -v ftm_conf_path=$ftm_conf_path '{
-                    if ($1 == board) {
+                    if ($1 == board) {  
                             print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" NR
-                            lineNumber=NR
+                            lineNumber=NR #store the current line number
                             if ($3 == 2){
                                 print "2G slot Instance -lineNumber" lineNumber "DTS board ID - "board_id_2g
-                                cmd = "sed -i " lineNumber"s" "\/" $2 "\/" board_id_2g "\/ " ftm_conf_path "/ftm.conf"
+                                # Construct sed command to replace board ID for 2G.
+                                # $2 contains existing board_id in ftm.conf file
+                                # board_id_2g contains the overridden board_id from dts file
+                                # In dts file, board_id will be 0x01 and caldata naming convention will be caldata_2.b0001. So, append 00 before board_id while constructing sed command
+                                # Same convention followed for 5G and 6G as well
+                                cmd = "sed -i " lineNumber"s" "\/" $2 "\/" "00" board_id_2g "\/ " ftm_conf_path "/ftm.conf"
                             }
                             if ($3 == 4){
                                 print "5G slot Instance -lineNumber" lineNumber "DTS board ID - "board_id_5g
+                                # Construct sed command to replace board ID for 5G
+                                # Same as 2G
                                 cmd = "sed -i " lineNumber"s" "\/" $2 "\/" "00" board_id_5g "\/ " ftm_conf_path "/ftm.conf"
                             }
                             else if($3 == 3)
                             {
                                 print "6G slot Instance -lineNumber" lineNumber "DTS board ID - "board_id_6g
+                                # Construct sed command to replace board ID for 6G
+                                # Same as 2G
                                 cmd = "sed -i " lineNumber"s" "\/" $2 "\/" "00" board_id_6g "\/ " ftm_conf_path "/ftm.conf"
                             }
                             system(cmd)
